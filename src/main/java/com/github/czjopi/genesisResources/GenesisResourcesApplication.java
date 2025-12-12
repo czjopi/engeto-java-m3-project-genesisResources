71c71bfa5780b2f377745ac5878c3372fbc7ed80
= <=>
2025-12-13 00:09:13 +0100

    init: add

---
+++ src/main/java/com/github/czjopi/genesisResources/GenesisResourcesApplication.java
@@ -0,0 +1,13 @@
+package com.github.czjopi.genesisResources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesisResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenesisResourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/czjopi/genesisResources/GenesisResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.czjopi.genesisResources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesisResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
