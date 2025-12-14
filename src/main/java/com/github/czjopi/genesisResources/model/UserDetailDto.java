94d02ea72fe0a7bffea09a3639b3415c7a25dba5
= <=>
2025-12-14 18:25:22 +0100

    feat(model): add User detailed info DTO

---
+++ src/main/java/com/github/czjopi/genesisResources/model/UserDetailDto.java
@@ -0,0 +1,53 @@
+package com.github.czjopi.genesisResources.model;
+
+/**
+ * Data Transfer Object (DTO) representing a detailed version of a user. Contains all identification
+ * data.
+ */
+public class UserDetailDto {
+    private int id;
+    private String name;
+    private String surname;
+    private String personId;
+    private String uuid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+}
