442029c337d0ba87bd411566dab8ac24d4eb8480
= <=>
2025-12-14 19:03:18 +0100

    feat(model): add User create DTO

---
+++ src/main/java/com/github/czjopi/genesisResources/model/UserCreateDto.java
@@ -0,0 +1,30 @@
+package com.github.czjopi.genesisResources.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * Data Transfer Object (DTO) used for creating a new user.
+ */
+public class UserCreateDto {
+    @NotBlank(message = "Name must not be blank")
+    private String name;
+
+    private String surname;
+
+    @NotBlank(message = "Person ID must not be blank")
+    @Size(min = 12, max = 12, message = "Person ID must be exactly 12 characters long")
+    private String personId;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+}
