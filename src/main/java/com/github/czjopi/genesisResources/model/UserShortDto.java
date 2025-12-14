fad3fff3e34472d71a1217c9ceb643f9d0e80414
= <=>
2025-12-14 17:54:20 +0100

    feat(model): add User short info DTO

---
+++ src/main/java/com/github/czjopi/genesisResources/model/UserShortDto.java
@@ -0,0 +1,35 @@
+package com.github.czjopi.genesisResources.model;
+
+/**
+ * Data Transfer Object (DTO) representing a short version of a user. Contains only basic
+ * identification data.
+ */
+public class UserShortDto {
+    private int id;
+    private String name;
+    private String surname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
