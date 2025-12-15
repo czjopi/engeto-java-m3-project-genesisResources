a3dc641fbb50ae5218a838c8767b94e2efd719ef
= <=>
2025-12-15 20:55:15 +0100

    feat(mapper): add UserMapper for DTO conversion

---
+++ src/main/java/com/github/czjopi/genesisResources/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package com.github.czjopi.genesisResources.mapper;
+
+import com.github.czjopi.genesisResources.model.User;
+import com.github.czjopi.genesisResources.model.UserDetailDto;
+import com.github.czjopi.genesisResources.model.UserShortDto;
+
+/**
+ * Mapper class for converting User entities to DTOs.
+ */
+public class UserMapper {
+
+    public static UserShortDto toUserShortDto(User user) {
+        UserShortDto dto = new UserShortDto();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        dto.setSurname(user.getSurname());
+        return dto;
+    }
+
+    public static UserDetailDto toUserDetailDto(User user) {
+        UserDetailDto dto = new UserDetailDto();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        dto.setSurname(user.getSurname());
+        dto.setPersonId(user.getPersonId());
+        dto.setUuid(user.getUuid());
+        return dto;
+    }
+
+}
