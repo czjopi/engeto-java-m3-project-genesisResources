00e82a39a00d283bed34b043a3ab24b09cbbbbe9
= <=>
2025-12-16 22:27:46 +0100

    fix(model): User update DTO
    - add correct ID validation

--- src/main/java/com/github/czjopi/genesisResources/model/UserUpdateDto.java
+++ src/main/java/com/github/czjopi/genesisResources/model/UserUpdateDto.java
@@ -1,12 +1,15 @@
 package com.github.czjopi.genesisResources.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 /**
  * Data Transfer Object (DTO) used for updating an existing user.
  */
 public class UserUpdateDto {
-    @NotBlank(message = "ID must not be blank")
+    @NotNull(message = "ID must not be blank")
+    @Positive(message = "ID must be a positive integer")
     private int id;
 
     @NotBlank(message = "Name must not be blank")
